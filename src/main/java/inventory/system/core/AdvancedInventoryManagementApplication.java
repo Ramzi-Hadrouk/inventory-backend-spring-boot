691644446dbe691dbd93b3926fb4f043e9ac542d
Ramzi-Hadrouk <hadrouk_ramzi_@hotmail.com>
2025-05-23 12:15:57 +0100

    main code

---
+++ src/main/java/inventory/system/core/AdvancedInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package inventory.system.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inventory/system/core/AdvancedInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package inventory.system.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
