4aa7d4ef480cd4bed4a852e441bc538cffb57132
Ramzi-Hadrouk <hadrouk_ramzi_@hotmail.com>
2025-05-23 23:28:45 +0100

    feat : Implement User and CustomUserDetailsService classes with UserRepository for user management

---
+++ src/main/java/inventory/system/core/User/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package inventory.system.core.User;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+@Override
+public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepo.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+    return org.springframework.security.core.userdetails.User
+            .withUsername(user.getEmail()) // use email here
+            .password(user.getPassword())
+            .roles(user.getRole().name())
+            .build();
+}
+}
