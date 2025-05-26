86c6cdd8fde7c6665b6bd360f6d261764241e07e
Ramzi-Hadrouk <hadrouk_ramzi_@hotmail.com>
2025-05-26 23:05:33 +0100

    feat: add Category and Product repository interfaces

---
+++ src/main/java/inventory/system/core/category/CategoryRepository.java
@@ -0,0 +1,5 @@
+package inventory.system.core.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
---
+++ src/main/java/inventory/system/core/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package inventory.system.core.product;
+
+import org.springframework.data.domain.Page;
+ 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
